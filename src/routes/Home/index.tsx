e8ebc030e2547c3c887a39467f8895e0fc8d664a
rafaelspa <rsdepaula67@gmail.com>
2023-01-12 09:52:51 -0300

    Comment Section

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header";
-import HomeCard from "../../components/HomeCard";
+import CarCard from "../../components/CarCard";
 import "./styles.css";
+import CommentCard from "../../components/CommentCard";
 
 export default function Home() {
   return (
@@ -8,11 +9,21 @@ export default function Home() {
       <Header />
       <main className="dsc-home-main">
         <section id="dsc-home-images-section">
-          <h1 className="dsc-home-title">Venha nos visitar</h1>
-          <div className="dsc-home-cards">
-            <HomeCard/>
-            <HomeCard/>
+          <h1 className="dsc-images-title">Venha nos visitar</h1>
+          <div className="dsc-images-cards">
+            <CarCard/>
+            <CarCard/>
           </div>
+        </section>
+        <section id="dsc-home-comments-section">
+            <h1 className="dsc-comments-title">O que estão dizendo</h1>
+            <div className="dsc-comments-cards">
+                <CommentCard/>
+                <CommentCard/>
+                <CommentCard/>
+                <CommentCard/>
+                <CommentCard/>
+            </div>
         </section>
       </main>
     </>
