44cbf9d5579e472be113a66245ad42968ca35bbf
rafaelspa <rsdepaula67@gmail.com>
2023-01-12 07:58:01 -0300

    Home page Start

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,18 @@
-import Header from '../../components/Header';
-import './styles.css';
+import Header from "../../components/Header";
+import "./styles.css";
 
 export default function Home() {
-    
-    return(
-        <Header/>
-    )
+  return (
+    <>
+      <Header />
+      <main className="dsc-home-main">
+        <section id="dsc-home-images-section">
+          <h1 className="dsc-home-title">Venha nos visitar</h1>
+          <div className="dsc-home-cards">
+            
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
